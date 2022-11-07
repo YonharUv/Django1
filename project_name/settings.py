493e085b5c57a5b4abfb7e3e0805ad5505ff3a46
yonhar_uv <danilinnicita@yandex.ru>
2022-11-07 21:20:59 +0300

    first_Django

--- project_name/settings.py
+++ project_name/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for project_name project.
 
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_project_name') ,
     }
 }
 
